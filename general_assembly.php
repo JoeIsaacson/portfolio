88dd1bcecb2859516f302a07b76c43135cd35f99
Joey Isaacson <joeisaacson23@gmail.com>
2013-10-02 14:15:55 -0400

    repo created

---
+++ general_assembly.php
@@ -0,0 +1,112 @@
+
+<!DOCTYPE html>
+<!-- Hi GA, yes I designed this-->
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Hello General Assembly</title>
+    
+    <?php 
+        include_once "header_tags.php"; 
+    ?>
+
+
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top hidden-phone">
+      <div class="navbar-inner">
+        <div class="container">
+            <a  href="index.php">Joey Isaacson</a>
+          
+            <ul class="nav pull-right">
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+
+    <div id="introduction">
+    <div class="container">
+      <div class="row">
+          <div class="span10 offset1">
+            <h1 class="header">Hello General Assembly.<br><br> Before this goes any further, I just want to say <span class="bold">thank you.</span></h1>
+          </div>
+      </div>
+
+      
+      <div class="row hidden-phone">
+         <!--<div class="span9">      
+              <a class="btn btn-who btn-large" href="#contact">Who I am</a>
+              <a class="btn btn-work btn-large" href="#contact">My Work</a>
+          </div>-->
+      </div> 
+
+        <div id="icons" class="row">
+          <div class="span9 hidden-phone">
+          </div>  
+      </div>
+    </div>  
+    </div> 
+
+
+
+     <div class="section" id="work_overview">
+      <hr>
+      <div class="container">
+        <hr class="visible-phone">
+        <div class="row">
+          <div class="span10 offset1">
+            <h2 class="weak">I have always been a curious person.</h2>
+            <h2 class="weak">I have always had a good eye for design.</h2>
+            <h2 class="weak">I have always paid attention to detail.</h2>
+            <h2 class="weak">I have always wanted to these skills to use.</h2>
+            <br>
+            <h2>But the UX design class at General Assembly SF taught me the right tools to develop a design process that solves real-life problems. For that, I thank you.</h2>
+            <br>
+            <h2 class="weak">If you are still searching for a Product Designer in New York, I would love to grab coffee.</h2>
+            </h2>
+          </div>
+        </div>  
+        </div>
+      </div>
+     </div>    
+
+    
+    <div class="container">
+      <div class="general_assembly row center">
+        <a href="index.php" class="btn btn-large btn-danger">See my recent work</a>
+      </div>
+    </div>
+    
+
+    <div class="section hidden-phone" id="contact">
+        <div class="container">
+            <h3>Available for coffee?<span class="weak"> Contact me via email at <span class="btn btn-primary">joeisaacson23@gmail.com</span></h3>
+        </div> 
+      </div>
+
+
+    <!-- Le javascript
+    ================================================== -->
+    
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+    <script src="js/jquery.stacktable.js"></script>
+    <script src="http://vjs.zencdn.net/c/video.js"></script>
+    <script src="js/application.js"></script>
+
+
+  
+
+</body></html>
---
+++ navbar.php
@@ -0,0 +1,16 @@
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+            <div class="row">
+            <div class="span10 offset1">  
+            <a class="brand" href="index.php">Joey Isaacson</a>
+
+            <ul class="nav pull-right hidden-phone">
+              <li><a href="index.php#work">Work</a><li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
