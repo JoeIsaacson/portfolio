1196d97041859f653eb57b500a7b7c958f3bf8a2
Joey Isaacson <joeisaacson23@gmail.com>
2013-10-06 22:41:39 -0400

    first commit

---
+++ healthyout.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<!-- Thanks to everyone at Houseplans.com. You know who you are. -->
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>UX Designer For Hire : Joey Isaacson</title>
+    
+    <?php 
+        include_once "header_tags.php"; 
+    ?>
+  
+  </head>
+
+  <body>
+
+  <?php 
+        include_once "navbar.php"; 
+  ?>
+
+
+   <div id="healthyout">
+   <div class="jumbotron">
+   <div class="container">
+      <div class="row">
+        <div class="span5 offset1">
+            <h1>HealthyOut</h1>
+            <h3>Interactive Mobile Designer</h3>
+            <hr>
+            <h3 class="weak paragraph-font">Create your own, flexible healthy diet by searching local restaurant dishes.</h3>
+            <a href="https://itunes.apple.com/us/app/healthyout-free-restaurant/id566409966?mt=8">
+              <img id="appstore" src="img/icons/appstore.png">
+            </a>
+        </div>  
+
+        <div class="span4 offset1 hidden-phone">
+            <img src="img/healthyout/iphone-mock.png">
+        </div>  
+      </div>
+   </div> 
+   </div>   
+
+      
+    <div class="container">
+    <div class="row paragraph-overview">
+        <div class="span10 offset1 overview paragraph-font secondary-color">
+          <h3 class="weak">Working closely with the co-founder of HealthyOut, <a href="https://twitter.com/wdot">@wdot</a>, we were able to take HealthyOut from a rough, low-fidelity wireframe to a polished app that made it into the top 10 food apps on the Apple AppStore.</h3> 
+          <br>
+          <h3 class="weak">Below are the high fidelity wireframes and visual designs for the On-boarding process and Search process that started from rough concepts on napkins, envelopes and discussions via Skype.</h3>
+        </div> 
+    </div>
+    
+    <div class="row image-overview">    
+
+
+        <div class="span10 offset1">
+          <br>
+          <h3>Onboarding User Flow</h3>
+          <p>Four step process for signing up for HealthyOut via Facebook</p>
+          <hr>
+        </div>  
+
+        <div class="span10 offset1 center image">
+            <img src="img/healthyout/WF-onboard.png">
+        </div>
+
+        <div class="span10 offset1 center image hidden">
+            <img src="img/healthyout/HF-onboard.png">
+        </div>
+
+        <div class="span10 offset1">
+          <div class="btn btn-large btn-info pull-right show-real-pixels">
+            <span>show real pixels</span>
+            <span class="hidden">show wireframes</span>
+          </div>    
+        </div> 
+    </div>
+    
+    <div class="row image-overview">     
+
+        <div class="span10 offset1">
+          <h3>Meal Search User Flow</h3>
+          <p>Searching and filtering dishes by food type, delivery type and restaurant availability was all possible with one screen.</p>
+          <hr>
+        </div> 
+
+        <div class="span10 offset1 center not-hidden image">
+            <img src="img/healthyout/WF-search.png">
+        </div>
+
+        <div class="span10 offset1 center hidden image">
+            <img src="img/healthyout/HF-search.png">
+        </div>
+
+        <div class="span10 offset1">
+          <div class="btn btn-large btn-info pull-right show-real-pixels">
+            <span>show real pixels</span>
+            <span class="hidden">show wireframes</span>
+          </div>    
+        </div> 
+
+    </div> 
+
+    </div>
+    </div>
+
+    <div class="section" id="contact">
+        <div class="container">
+           <?php 
+                include "footer.php";
+             ?>
+        </div> 
+    </div>
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script type="text/javascript" src="js/jquery.ui.touch-punch.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap-select.js"></script>
+    <script type="text/javascript" src="js/bootstrap-switch.js"></script>
+    <script type="text/javascript" src="js/flatui-checkbox.js"></script>
+    <script type="text/javascript" src="js/flatui-radio.js"></script>
+    <script type="text/javascript" src="js/jquery.tagsinput.js"></script>
+    <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+    <script type="text/javascript" src="js/jquery.stacktable.js"></script>
+    <script type="text/javascript" src="js/application.js"></script>
+    <script type="text/javascript" src="js/modernizr.js"></script>
+    <script type="text/javascript" src="js/jquery.cslider.js"></script>
+
+</body></html>
