54c579efc1cb4ceda919975a60bab97c7ee0158f
Joey Isaacson <joeisaacson23@gmail.com>
2014-08-31 16:56:05 -0400

    GA Unique ID Script Added

--- footer.php
+++ footer.php
@@ -5,6 +5,6 @@
 
 <div class="row">
 	<div class="span12">
-		<h3>Need a UX designer?<span class="weak"> e-mail me at <span class="btn btn-primary">joeisaacson23@gmail.com</span></h3>
+		<h3>Need a UX designer?<span class="weak"> e-mail me at <span class="btn btn-primary">joey.a.isaacson@gmail.com</span></h3>
 	</div>
 </div>
