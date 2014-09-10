52521f5d2ac01b88cc332207d7c03116f7eb1b86
Joey Isaacson <joeisaacson23@gmail.com>
2014-09-09 21:26:21 -0400

    pixel reveal fixed

--- app/views/work/work-controller.js
+++ app/views/work/work-controller.js
@@ -4,16 +4,17 @@ angular.module('myApp.work-controller', ['ngRoute'])
 
 .controller('workController', ['$scope', function($scope) {
 
-    console.log('controller working!');
+
+    $scope.wireframeOne = true;
+    $scope.wireframeTwo = true;
 
     $scope.showWireframes = function(wireframeNumber) {
-        console.log('show wireframes!');
         console.log(wireframeNumber);
-        $scope.wireframeStatus =! wireframeNumber 
-    };
-
-    $scope.wireframeStatus = function(wireframeNumber) {
-
+        if (wireframeNumber === 'wireframeOne') {
+            $scope.wireframeOne = !$scope.wireframeOne;
+        } else {
+            $scope.wireframeTwo = !$scope.wireframeTwo;
+        };
     };
 
 
