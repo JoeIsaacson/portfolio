93aac7e3fa5a4179cb4986e2c173734b701d73d8
Joey Isaacson <joeisaacson23@gmail.com>
2015-01-17 13:15:02 -0500

    JS animation disabled on mobile

--- js/js.js
+++ js/js.js
@@ -1,7 +1,16 @@
-$(window).scroll(function() {
-    var scrollLength = $(document).scrollTop();
+$(document).ready(function() {
 
-    console.log("length is " + scrollLength);
-    $("#jumbotron--header").css({"margin-top": - (scrollLength * 2.0) + 'px'});
-    $("#header--img").css({"margin-top": - (scrollLength * 1.0) + 'px'});
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        console.log("jQuery animation disabled for mobile");
+    } else {
+
+        $(window).scroll(function() {
+            var scrollLength = $(document).scrollTop();
+            console.log("length is " + scrollLength);
+
+            $("#jumbotron--header").css({"margin-top": - (scrollLength * 2.0) + 'px'});
+            $("#header--img").css({"margin-top": - (scrollLength * 1.0) + 'px'});
+        });
+    }
+    
 });
