4f7b102a60ed5e5b457a0db7fc58c905f2222ea0
joeisaacson <joey.a.isaacson@gmail.com>
2015-10-27 01:10:35 -0400

    loader added

--- js/js.js
+++ js/js.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        console.log("jQuery animation disabled for mobile");
+        //console.log("jQuery animation disabled for mobile");
     } else {
 
         $(window).scroll(function() {
             var scrollLength = $(document).scrollTop();
-            console.log("length is " + scrollLength);
+            //console.log("length is " + scrollLength);
 
             $("#jumbotron--header").css({"margin-top": - (scrollLength * 2.0) + 'px'});
             $("#header--img").css({"margin-top": - (scrollLength * 1.0) + 'px'});
         });
     }
-    
+
 });
