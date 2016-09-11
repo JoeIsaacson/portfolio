0bce33957e9e0d2b750ddce7aac264589cc4f538
joeisaacson <joey.a.isaacson@gmail.com>
2016-09-11 23:10:48 +0100

    cleanup

--- js/js.js
+++ js/js.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 
+    $(".message").hide();
+
+    function showLoaderMessage() {
+      $(".message").fadeIn(500);
+      console.log("show message");
+    };
+
+    setTimeout(showLoaderMessage, 2000);
+
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        //console.log("jQuery animation disabled for mobile");
+        console.log("jQuery animation disabled for mobile");
     } else {
 
         $(window).scroll(function() {
             var scrollLength = $(document).scrollTop();
-            //console.log("length is " + scrollLength);
-
             $("#jumbotron--header").css({"margin-top": - (scrollLength * 2.0) + 'px'});
             $("#header--img").css({"margin-top": - (scrollLength * 1.0) + 'px'});
         });
